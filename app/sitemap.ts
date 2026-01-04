a2b8f84b70d25cfd220eebd5189007f3060f40f9
arghosts <arghost.viles@gmail.com>
2026-01-04 08:29:47 +0700

    Fix sitemap location and force dynamic

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://jagopilih.vercel.app";
 
